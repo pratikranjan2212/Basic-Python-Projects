800bbd979d4a1c7faf5408f4bd1a101554e2f641
Pratik Ranjan <128955674+pratikranjan2212@users.noreply.github.com>
2024-07-12 07:32:22 +0000

    display notification added

--- Drink Water Reminder.py
+++ Drink Water Reminder.py
@@ -1,9 +1,17 @@
 import pyttsx3
+from plyer import notification
 import time
 
 engine=pyttsx3.init()
 def drink_water_reminder():
     while True:
+        # Display a notification to drink water
+        notification.notify(
+            title = "Please Drink Water Now",
+            message = "Drinking water is essential to keep your body hydrated and healthy. Please drink water now.",
+            app_icon = "C:\\Users\\priti\\Downloads\\aqua.ico",
+            timeout = 10 # The notification will be displayed for 10 seconds
+        )
         engine.say("Please drink water")
         engine.runAndWait()
         # Wait for one hour
